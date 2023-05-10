8394b4b2c52b7052103014b9f43edcd77b765541
KevSalcedo <kevsalcedom@gmail.com>
2023-05-10 14:51:31 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Test task"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _backgroundColor = Colors.red;
+
+  void _changeColor() {
+    setState(
+      () {
+        _backgroundColor = Color.fromRGBO(
+          Random().nextInt(256),
+          Random().nextInt(256),
+          Random().nextInt(256),
+          1,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _changeColor,
+      child: Scaffold(
+        backgroundColor: _backgroundColor,
+        body: const Center(
+          child: Text(
+            "Hey there",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
