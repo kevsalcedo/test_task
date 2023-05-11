2a96604df10d85631e788816a24a2b7ae33a2e7e
KevSalcedo <kevsalcedom@gmail.com>
2023-05-11 12:11:14 -0500

    create void _showDialog()

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,102 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            int _radioValue = 0;
+            bool _switchValue1 = false;
+            bool _switchValue2 = false;
+
+            return AlertDialog(
+              title: const Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    const Text(
+                      'Select colors',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    RadioListTile<int>(
+                      title: const Text('Party mode'),
+                      value: 0,
+                      groupValue: _radioValue,
+                      onChanged: (int? value) {
+                        setState(() => _radioValue = value!);
+                      },
+                    ),
+                    RadioListTile<int>(
+                      title: const Text('Warm colors'),
+                      value: 1,
+                      groupValue: _radioValue,
+                      onChanged: (int? value) {
+                        setState(() => _radioValue = value!);
+                      },
+                    ),
+                    RadioListTile<int>(
+                      title: const Text('Cold colors'),
+                      value: 2,
+                      groupValue: _radioValue,
+                      onChanged: (int? value) {
+                        setState(() => _radioValue = value!);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Enable options',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SwitchListTile(
+                      title: const Text('Sound'),
+                      value: false,
+                      onChanged: (bool value) {},
+                    ),
+                    SwitchListTile(
+                      title: const Text('Dark mode'),
+                      value: false,
+                      onChanged: (bool value) {},
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _changeColor,
       child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Test task"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                _showDialog();
+              },
+            ),
+          ],
+        ),
         backgroundColor: _backgroundColor,
         body: const Center(
           child: Text(
