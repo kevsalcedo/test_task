2081ae57eca6d48ae4d99ce0914487ea520b5cdf
KevSalcedo <kevsalcedom@gmail.com>
2023-05-12 13:34:44 -0500

    Create style for settings text titles.

---
+++ lib/utils/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'display_constants.dart';
+
+const kTextStyle = TextStyle(
+  fontSize: kHeadlineType2,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Roboto',
+  letterSpacing: kLetterSpacing,
+);
