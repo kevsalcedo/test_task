4c241a2d78faa4a9f93f68fe067aeca9fd6082d2
KevSalcedo <kevsalcedom@gmail.com>
2023-05-12 12:59:12 -0500

    add constants

---
+++ lib/utils/display_constants.dart
@@ -0,0 +1,14 @@
+const String kHeyThere = 'Hey there';
+const String kAppTitle = 'Test task';
+const String kSettings = 'Settings';
+const double kHeadlineType1 = 22.0;
+const double kHeadlineType2 = 18.0;
+const String kSelectColors = 'Select colors';
+const String kPartyMode = 'Party mode';
+const String kWormMode = 'Warm mode';
+const String kColdMode = 'Cold mode';
+const String kEnableOptions = 'Enable options';
+const String kSound = 'Sound';
+const String kDarkMode = 'Dark mode';
+const String kNote1 = 'note1.wav';
+const int kSoundDuration = 1200;
